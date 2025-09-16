7453d7966c5ec5785d937b1f5a85d6f2424686b9
IT24100020 <it24100020@my.sliit.lk>
2025-09-16 16:00:37 +0530

    Add IT24100020_Lab07 to Lab Sheet 07

---
+++ Lab Sheet 07/IT24100020_Lab07.R
@@ -0,0 +1,11 @@
+getwd()
+setwd("C:\\Users\\pooja\\Desktop\\PS\\IT24100020_Lab07")
+getwd()
+
+punif(25, min=0, max=40) - punif(10, min=0, max=40)
+
+pexp(2, rate=1/3)
+
+pnorm(130, mean=100, sd=15, lower.tail=FALSE)
+
+qnorm(0.95, mean=100, sd=15)
